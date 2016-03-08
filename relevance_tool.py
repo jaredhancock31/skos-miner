97643e48bece70be4f8c5a52ef5b927cf7f219af
jared <jaredhancock31@gmail.com>
2016-03-07 23:33:44 -0600

    simplifed importance metric, normalized values, cleaned

--- relevance_tool.py
+++ relevance_tool.py
@@ -1,33 +1,65 @@
-import sys
+"""
+    Author: Jared Hancock
+    Date: March 2016
+    Email: jaredhancock31@gmail.com
+"""
 import corpus_frequency
 import thesaurus_related
 from collections import OrderedDict
 
 
+def get_most_important_concept(scores):
+    return max(scores.iterkeys(), key=lambda key: scores[key])
+
+
+def normalize_on_max(scores, hi_score=None):
+    if hi_score is None:
+        hi_score = get_most_important_concept(scores)
+
+    for key in scores:
+        scores[key] = round((float(scores.get(key)) / hi_score), 4)
+
+    return scores
+
+
+def normalize_on_sum(scores, total=None):
+    if total is None:
+        return None
+    for key in scores:
+        scores[key] = round((float(scores.get(key)) / total), 4)
+
+
 def main():
     relateds = thesaurus_related.get_relateds()
     frequencies = corpus_frequency.get_all_frequencies()
-    relevance = {}
+    relevance = {}              # has all metrics associated with a key
+    importance_scores = {}      # importance metric only value for key
+    total_imp = 0               # sum of total importance in case we want to normalize against the sum
+    max_score = 0
 
     for label, num_related in relateds.items():
-        # print label, str(type(label))
         if label in frequencies:
-            # sys.stdout.write(':)')
             freq = frequencies.get(label)   # get frequency from corpus list
-            importance_score = round((0.3 * num_related) + (0.7 * freq), 4)
-            relevance[label] = {'related': num_related, 'frequency': freq, 'importance': importance_score}
-        # else:
-        #     sys.stdout.write('~')
+            importance = round(((0.3 * num_related) + (0.7 * freq)), 4)
+            total_imp += importance
 
-    # for i in relevance:
-    #     imp = relevance.get(i)
-    #     print i + ': ' + str(imp['importance'])
+            if importance > max_score:
+                max_score = importance
 
-    ordered_rel = OrderedDict(sorted(relevance.iteritems(), reverse=True, key=lambda x: x[1]['importance']))
-    # print ordered_rel['machine tool']
+            importance_scores[label] = importance
 
-    for concept in ordered_rel:
-        print concept + ": " + str(ordered_rel[concept]['importance'])
+            # ********************** uncomment line below if you want to keep a more comprehensive list ****************
+            # relevance[label] = {'related': num_related, 'frequency': freq, 'importance': importance}
+
+    # sort the dict by highest importance
+    # ordered_rel = OrderedDict(sorted(relevance.iteritems(), reverse=True, key=lambda x: x[1]['importance']))
+    # for concept in ordered_rel:
+    #     print concept + ": " + str(ordered_rel[concept]['importance'])
+
+    importance_scores = normalize_on_max(importance_scores, max_score)
+
+    for key in importance_scores:
+        print key + ' : ' + str(importance_scores.get(key))
 
 
 if __name__ == "__main__":
